cf700f7f5cc9507d8ab5bb366ff9157a412aeae3
Mayonnaise-Slap <368758@niuitmo.ru>
2024-04-02 14:11:24 +0300

    yolo2

---
+++ RetailThingey/src/Presentation/RetailThingey.Presentation.Http/Controllers/CartController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using RetailThingey.Application.Extensions.Buisnesslogic;
+using RetailThingey.Application.Models.Product;
+
+namespace RetailThingey.Presentation.Http.Controllers;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        [HttpPost]
+        public ActionResult AddToCart(ProductModel productModel, int quantity)
+        {
+            _cartService.AddToCart(productModel, quantity);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult RemoveFromCart(ProductModel productModel)
+        {
+            _cartService.RemoveFromCart(productModel);
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult UpdateQuantity(ProductModel productModel, int newQuantity)
+        {
+            _cartService.UpdateQuantity(productModel, newQuantity);
+            return Ok();
+        }
+
+        [HttpGet("/")]
+        public ActionResult<List<ProductModel>> GetCartItems()
+        {
+            var cartItems = _cartService.GetCartItems();
+            return Ok(cartItems);
+        }
+
+        [HttpGet("/total_price")]
+        public ActionResult<decimal> GetTotalPrice()
+        {
+            var totalPrice = _cartService.GetTotalPrice();
+            return Ok(totalPrice);
+        }
+    }
---
+++ RetailThingey/src/Presentation/RetailThingey.Presentation.Http/Controllers/DeliveryPointController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using RetailThingey.Application.Extensions.Buisnesslogic;
+using RetailThingey.Application.Models.Shop;
+
+namespace RetailThingey.Presentation.Http.Controllers;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class DeliveryPointController : ControllerBase
+    {
+        private readonly IDeliveryPointService _deliveryPointService;
+
+        public DeliveryPointController(IDeliveryPointService deliveryPointService)
+        {
+            _deliveryPointService = deliveryPointService;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<DeliveryPoint> Get(int id)
+        {
+            var dp = _deliveryPointService.GetDeliveryPointById(id);
+            if (dp == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(dp);
+        }
+
+        [HttpPost]
+        public ActionResult<DeliveryPoint> Post(DeliveryPoint deliveryPoint)
+        {
+            _deliveryPointService.AddDeliveryPoint(deliveryPoint);
+            return CreatedAtAction(nameof(Get), new { id = deliveryPoint.Id }, deliveryPoint);
+        }
+
+        [HttpGet]
+        public ActionResult<List<DeliveryPoint>> GetAll()
+        {
+            var deliveryPoints = _deliveryPointService.GetAllDeliveryPoints();
+            return Ok(deliveryPoints);
+        }
+    }
